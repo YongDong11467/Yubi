83d8490a1abebcd355fc9f69213099ea483928dd
YongDong11467 <yongdong11467@gmail.com>
2021-05-24 13:37:15 -0400

    added some algpage layout

--- client/src/pages/Algorithms/AlgorithmsPage.js
+++ client/src/pages/Algorithms/AlgorithmsPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import AlgInfo from '../../components/AlgInfo'
-import { ListGroup } from 'react-bootstrap';
+import { Container, ListGroup } from 'react-bootstrap';
+
+import './algorithmspage.css'
 
 export default class AlgorithmsPage extends Component {
 
@@ -18,23 +20,30 @@ export default class AlgorithmsPage extends Component {
     render() {
         return (
             <div>
-                <ListGroup defaultActiveKey="#link1">
-                    {this.state.algorithms.map(alg => (
-                        <ListGroup.Item action href="#link1">
-                        {alg}
-                        </ListGroup.Item>
-                    ))}
-                    {/* <ListGroup.Item action href="#link1">
-                    Link 1
-                    </ListGroup.Item>
-                    <ListGroup.Item action href="#link2" disabled>
-                    Link 2
-                    </ListGroup.Item>
-                    <ListGroup.Item action onClick={() => this.algorithmClicked}>
-                    This one is a button
-                    </ListGroup.Item> */}
-                </ListGroup>
-                <AlgInfo />
+                <Container>
+                    <div className="split sidebar">
+                        <ListGroup defaultActiveKey="#link1">
+                            {this.state.algorithms.map(alg => (
+                                <ListGroup.Item action href="#link1" key={alg}>
+                                {alg}
+                                </ListGroup.Item>
+                            ))}
+                            {/* <ListGroup.Item action href="#link1">
+                            Link 1
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="#link2" disabled>
+                            Link 2
+                            </ListGroup.Item>
+                            <ListGroup.Item action onClick={() => this.algorithmClicked}>
+                            This one is a button
+                            </ListGroup.Item> */}
+                        </ListGroup>
+                    </div>
+
+                    <div className="split algdetails">
+                        <AlgInfo />
+                    </div>
+                </Container>
             </div>
         )
     }
