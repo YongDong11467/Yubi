487ef9ffb5f4b629de45a54234c7a550671cef7f
YongDong11467 <yongdong11467@gmail.com>
2021-05-31 14:09:45 -0400

    establish connection to mongo

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express(),
       bodyParser = require("body-parser");
       port = 3080;
+
+require("dotenv").config();
 
 const diningRouter = require("./routes/diningRoute");
 const quoteRouter = require("./routes/quoteRoute");
 
 app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
 app.use('/login', (req, res) => {
     res.send({
         token: 'test123'
